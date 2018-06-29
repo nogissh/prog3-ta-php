c3f36be54e9cb6ed2d98f6a1c80a4a6c02ccb248
Toshiki Ohnogi <thetoshiki0419@gmail.com>
2018-06-29 14:07:37 +0900

    fix: 名前で検索できるようにした

--- php/mysql/call_as_json.php
+++ php/mysql/call_as_json.php
@@ -3,8 +3,16 @@ try {
   // MySqlに接続
   $pdo=new PDO('mysql:host=133.78.83.191:13306;dbname=test_db;charset=utf8', 'testuser', '0000');
 
+  // SQL文を決定
+  if (isset($_GET['name'])) {
+    $sql_sentense = 'select * from test_table where name like "%'.$_GET['name'].'%"';
+  }
+  else {
+    $sql_sentense = 'select * from test_table';
+  }
+
   // クエリを取得
-  $q = $pdo->query('select * from test_table');
+  $q = $pdo->query($sql_sentense);
 
   // オブジェクトに整形
   $userdata = array();
